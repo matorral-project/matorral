c10c38967d217b84ba5f0e570cc360d4642f06da
Agustin Mendez <matagus@gmail.com>
2019-05-20 22:54:27 +0200

    Admin actions to manage epics, stories, tasks & sprints.

--- stories/admin.py
+++ stories/admin.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib import admin
+from django.utils import timezone
 
 from simple_history.admin import SimpleHistoryAdmin
 
@@ -33,6 +34,11 @@ class EpicAdmin(SimpleHistoryAdmin):
     list_select_related = ('state', 'owner')
     search_fields = ['title']
     form = EpicForm
+    actions = ['mark_as_done']
+
+    def mark_as_done(self, request, queryset):
+        count = queryset.update(completed_at=timezone.now(), state='dn')
+        self.message_user(request, f"{count} epics successfully maked as done")
 
 
 class StoryAdmin(SimpleHistoryAdmin):
@@ -42,12 +48,38 @@ class StoryAdmin(SimpleHistoryAdmin):
     list_select_related = ('epic', 'state', 'assignee')
     search_fields = ['title', 'epic__title', 'sprint__title', 'assignee__username']
     form = StoryForm
+    actions = ['reset_sprint', 'finish_sprint', 'mark_as_done']
+
+    def reset_sprint(self, request, queryset):
+        # given a set of stories it sets the sprint field to None
+        count = queryset.update(sprint=None)
+        self.message_user(request, f"{count} stories successfully reseted")
+
+    def finsh_sprint(self, request, queryset):
+        # given a set of stories it sets the sprint field to point to the next
+        # sprint instance
+        try:
+            next_sprint = Sprint.objects.get(completed_at__isnull=True)
+        except Sprint.DoesNotExist:
+            self.message_user(request, f"There's no current sprint. Please create one first.")
+        else:
+            count = queryset.update(sprint=next_sprint)
+            self.message_user(request, f"{count} stories moved to the next sprint")
+
+    def mark_as_done(self, request, queryset):
+        count = queryset.update(completed_at=timezone.now(), state='dn')
+        self.message_user(request, f"{count} stories successfully maked as done")
 
 
 class TaskAdmin(admin.ModelAdmin):
     actions_on_bottom = True
     list_display = ('title', 'created_at', 'completed_at')
     search_fields = ['title']
+    actions = ['mark_as_done']
+
+    def mark_as_done(self, request, queryset):
+        count = queryset.update(completed_at=timezone.now())
+        self.message_user(request, f"{count} tasks successfully maked as done")
 
 
 admin.site.register(Epic, EpicAdmin)
